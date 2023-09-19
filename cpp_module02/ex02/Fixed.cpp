b4bfa67d2c7cd3c9ea2eda055e1782b38ebc3d35
achraf <achrafsettar8@gmail.com>
2023-09-19 06:31:58 +0100

    Tue Sep 19 06:31:58 +01 2023

--- cpp_module02/ex02/Fixed.cpp
+++ cpp_module02/ex02/Fixed.cpp
@@ -2,40 +2,43 @@
 
 Fixed::Fixed(void) : Fixed_point(0)
 {
-	std::cout << "Default constructor called\n";
+	// std::cout << "Default constructor called\n";
 }
 
 Fixed::Fixed(const int n)
 {
-	std::cout << "Int constructor called\n";
+	// std::cout << "Int constructor called\n";
 	this->Fixed_point = n << fractional_bits;
 }
 Fixed::Fixed(const float n)
 {
-	std::cout << "Float constructor called\n";
+	// std::cout << "Float constructor called\n";
 	this->Fixed_point = roundf(n * (1 << fractional_bits));
 }
 
 Fixed::Fixed(Fixed const &other){
-	std::cout << "Copy constructor called\n";
+	// std::cout << "Copy constructor called\n";
 	*this = other;
 }
 
+
 int	Fixed::getRawBits(void) const
 {
-	std::cout << "getRawBits member function called\n";
+	// std::cout << "getRawBits member function called\n";
 	return (this->Fixed_point);
 }
 
-void Fixed::setRawBits(int const raw)
+
+void	Fixed::setRawBits(int const raw)
 {
-	std::cout << "setRawBits member function called\n";
+	// std::cout << "setRawBits member function called\n";
 	this->Fixed_point = raw;
 }
 
 float Fixed::toFloat(void) const
 {
 	float res = Fixed_point / (float)(1 << fractional_bits);
+	std::cout << Fixed_point << ' ' << res <<'\n';
 	return (res);
 }
 
@@ -45,19 +48,107 @@ int	Fixed::toInt(void) const
 }
 
 
-Fixed& Fixed::operator = (Fixed const &other)
-{
-	std::cout << "Copy assignement operator called\n";
+Fixed& Fixed::operator = (Fixed const &other){
+	// std::cout << "Copy assignement operator called\n";
 	this->Fixed_point = other.Fixed_point;
 	return (*this);
 }
 
 
-
-Fixed::~Fixed(void)
-{
-	std::cout << "Destructor called\n";
+bool	Fixed::operator < (Fixed const &a){
+	return (Fixed_point < a.Fixed_point);
 }
+
+bool	Fixed::operator > (Fixed const &a){
+	return (Fixed_point > a.Fixed_point);
+}
+
+bool	Fixed::operator >= (Fixed const &a){
+	return (Fixed_point >= a.Fixed_point);
+}
+
+bool	Fixed::operator <= (Fixed const &a){
+	return (Fixed_point <= a.Fixed_point);
+}
+
+bool	Fixed::operator == (Fixed const &a){
+	return (Fixed_point == a.Fixed_point);
+}
+
+bool	Fixed::operator != (Fixed const &a){
+	return (Fixed_point != a.Fixed_point);
+}
+
+int	Fixed::operator + (Fixed const &a){
+	return (Fixed_point + a.Fixed_point);
+}
+
+int	Fixed::operator - (Fixed const &a){
+	return (Fixed_point - a.Fixed_point);
+}
+
+float	Fixed::operator * (Fixed const &a){
+	float f1 = Fixed_point / (float)(1 << fractional_bits);
+	float f2 = a.Fixed_point / (float)(1 << fractional_bits);
+	return (f1 * f2);
+}
+
+int	Fixed::operator / (Fixed const &a){
+	return (Fixed_point / a.Fixed_point);
+}
+
+Fixed const& Fixed::max(Fixed const &a, Fixed const &b){
+	if (a.Fixed_point >= b.Fixed_point)
+		return (a);
+	return (b);
+}
+
+Fixed const& Fixed::min(Fixed const &a, Fixed const &b){
+	if (a.Fixed_point <= b.Fixed_point)
+		return (a);
+	return (b);
+}
+
+Fixed	Fixed::operator ++ (void){
+	++Fixed_point;
+	return (*this);
+}
+
+Fixed	Fixed::operator ++ (int){
+	Fixed copy = *this;
+	Fixed_point++;
+	return copy;
+}
+
+Fixed	Fixed::operator -- (void){
+	--Fixed_point;
+	return (*this);
+}
+
+Fixed	Fixed::operator -- (int){
+	Fixed copy = *this;
+	Fixed_point--;
+	return copy;
+}
+
+
+int& Fixed::max(int &fp1, int &fp2){
+	if (fp1 >= fp2)
+		return fp1;
+	return (fp2);
+}
+
+int& Fixed::min(int &fp1, int &fp2){
+	if (fp1 <= fp2)
+		return fp1;
+	return (fp2);
+}
+
+Fixed::~Fixed(void){
+	// std::cout << "Destructor called\n";
+}
+
+
 std::ostream& operator<<(std::ostream& os, const Fixed & F)
 {
 	os << F.toFloat();
--- cpp_module02/ex02/Fixed.hpp
+++ cpp_module02/ex02/Fixed.hpp
@@ -15,7 +15,28 @@ public:
 	void	setRawBits( int const raw );
 	float	toFloat(void) const;
 	int		toInt(void) const;
-	Fixed&	operator=(Fixed const &other);
+	Fixed&	operator = (Fixed const &other);
+	bool	operator < (Fixed const &a);
+	bool	operator > (Fixed const &a);
+	bool	operator >= (Fixed const &a);
+	bool	operator <= (Fixed const &a);
+	bool	operator == (Fixed const &a);
+	bool	operator != (Fixed const &a);
+	int		operator + (Fixed const &a);
+	int		operator - (Fixed const &a);
+	float	operator * (Fixed const &a);
+	int		operator / (Fixed const &a);
+	Fixed	operator ++ (void);
+	Fixed	operator ++ (int);
+	Fixed	operator -- (void);
+	Fixed	operator -- (int);
+	// int		operator / (Fixed const &a);
+	// int		operator / (Fixed const &a);
+	static Fixed const&	max(Fixed const &a, Fixed const &b);
+	static Fixed const& min(Fixed const &a, Fixed const &b);
+	static int& max(int &fp1, int &fp2);
+	static int& min(int &fp1, int &fp2);
+
 	~Fixed(void);
 };
 
